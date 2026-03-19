d9b05665e25a1c99b2d6075dbfb84273e1bf7955
drb <drb@qq.comgit config --global user.email drb@qq.com>
2026-03-19 19:59:11 +0800

    完成了注册页面开发

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
@@ -36,10 +36,10 @@ module.exports = {
     'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验 (props解构丢失响应式)
     // 添加未定义变量错误提示，create-vue@3.6.3 关闭，这里加上是为了支持下一个章节演示。
     'no-undef': 'error'
+  },
+  globals: {
+    ElMessage: 'readonly',
+    ElMessageBox: 'readonly',
+    ElLoading: 'readonly'
   }
-  // globals: {
-  // ElMessage: 'readonly',
-  // ElMessageBox: 'readonly',
-  // ElLoading: 'readonly'
-  // }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,8 @@
-<script setup>
-// vue3 中this不能直接使用
-// 1.获取路由对象 router useRouter
-//   const router = useRouter()
-// 2.获取路由参数 route
-//   const route = useRoute()
-
-import { useRouter, useRoute } from 'vue-router'
-import { useUserStore } from '@/stores' //因为合并了模块，所以直接引入即可
-
-const router = useRouter() // 获取路由对象
-const route = useRoute() // 获取路由参数
-
-const goList = () => {
-  // this.$router.push('/list')不能在setup中使用
-  console.log(router, route)
-  router.push('/list')
-}
-
-const userStore = useUserStore()
-</script>
+<script setup></script>
 
 <template>
   <div>
-    <hr />
     <router-view></router-view>
-    <hr />
-    <hr />
-    <hr />
-    我是app
-    <el-button @click="$router.push('/home')">跳首页</el-button>
-    <el-button @click="goList">跳列表页</el-button>
-
-    <p>当前token: {{ userStore.token }}</p>
-    <el-button @click="userStore.setToken('123456')">登录</el-button>
-    <el-button @click="userStore.removeToken">退出</el-button>
   </div>
 </template>
 
